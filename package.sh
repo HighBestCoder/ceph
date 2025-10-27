405bcae644b4c4667bd20d55e4cc7df817ede3f0
Your Name <you@example.com>
2025-10-27 09:56:28 +0000

    update package.sh
    Signed-off-by: Your Name <you@example.com>

--- package.sh
+++ package.sh
@@ -12,7 +12,9 @@ cp ./build/bin/ceph-bluestore-tool $PACK_DIR/bin/
 cp ./build/bin/ceph-objectstore-tool $PACK_DIR/bin/
 
 # 复制自定义库(非系统库)
-cp -rfL /lib64/libfmt.so.6.2.1 $PACK_DIR/lib/
+cp -rfL /lib/x86_64-linux-gnu/libsnappy.so.1 $PACK_DIR/lib/
+cp -rfL ./build/lib/libceph_snappy.so $PACK_DIR/lib/
+cp -rfL /lib64/libfmt.so.6.2.1 $PACK_DIR/lib/libfmt.so.6
 cp -rfL ./build/lib/libceph-common.so.2 $PACK_DIR/lib/
 
 # 创建启动脚本
